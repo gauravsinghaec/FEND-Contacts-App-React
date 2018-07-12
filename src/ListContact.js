df5279fb3136873306be70d95885e28cc39ff63c
gauravsinghaec <gauravsinghaec@gmail.com>
2018-07-12 07:01:30 +0530

    Render default contacts.

--- src/ListContact.js
+++ src/ListContact.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 class ListContact extends Component {
-	render(){
-		console.log(this.props);
-		return (
-			<ol className='contact-list'>
-			</ol>
-		);
-	}
+  render(){
+    return (
+      <ol className='contact-list'>
+      {this.props.contacts.map((contact) => (
+        <li key={contact.id} className='contact-list-item'>
+        <div className='contact-avatar'
+          style={{ backgroundImage: `url(${contact.avatarURL})`}}>
+        </div>
+        <div className='contact-details'>
+          <p>{contact.name}</p>
+          <p>{contact.email}</p>
+        </div>
+        <button className='contact-remove'>
+          Remove
+        </button>
+        </li>
+      ))}
+      </ol>
+    );
+  }
 }
 
 export default ListContact
