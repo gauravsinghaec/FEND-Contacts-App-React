b3ff00da8285edb0598575d59ce17f8387b015aa
gauravsinghaec <gauravsinghaec@gmail.com>
2018-07-14 16:14:23 +0530

    Add form-serialize package to capture data to be sent to server on submit.

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import ImageInput from './ImageInput';
+import serializeForm from 'form-serialize';
 
 class CreateContact extends Component{
+	handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(serializeForm(e.target));
+	}
 	render(){
 	    return (
 	    	<div className='create-contact'>
 	    		<Link to='/' className='close-create-contact'>Close</Link>
-	    		<form className='create-contact-form'>
+	    		<form onSubmit={this.handleSubmit} className='create-contact-form'>
 					<ImageInput className='create-contact-avatar-input'
 						name='avararURL'
 						maxHeight={64}
 					/>
 					<div className='create-contact-details'>
-					<input type='text' className='' name='name' placeholder='Name' value=''/>
-					<input type='text' className='' name='email' placeholder='Email' value=''/>
+					<input type='text' className='' name='name' placeholder='Name' />
+					<input type='text' className='' name='email' placeholder='Email' />
 					<button className='create-contact-button'>Add Contact</button>
 					</div>
 	    		</form>
