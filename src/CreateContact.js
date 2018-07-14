2581454d72b6d3df31095b6a9526f1cf3abde78b
gauravsinghaec <gauravsinghaec@gmail.com>
2018-07-14 13:32:53 +0530

    Add form elements to add contacts.

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import ImageInput from './ImageInput';
 
 class CreateContact extends Component{
 	render(){
 	    return (
 	    	<div className='create-contact'>
-	    		Create Contact
+	    		<Link to='/' className='close-create-contact'>Close</Link>
+	    		<form className='create-contact-form'>
+					<ImageInput className='create-contact-avatar-input'
+						name='avararURL'
+						maxHeight={64}
+					/>
+					<div className='create-contact-details'>
+					<input type='text' className='' name='name' placeholder='Name' value=''/>
+					<input type='text' className='' name='email' placeholder='Email' value=''/>
+					<button className='create-contact-button'>Add Contact</button>
+					</div>
+	    		</form>
 	    	</div>
 	    );
 	}
